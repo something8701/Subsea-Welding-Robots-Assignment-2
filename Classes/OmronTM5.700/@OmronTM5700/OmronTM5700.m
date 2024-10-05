7bfa02bcaa1f4c26e1cf4eeb8e587a595053d250
VKila24 <160553306+VKila24@users.noreply.github.com>
2024-10-05 22:00:33 +1000

    Update Omron CAD Code
    Update OmronTM5700 class code.
    Add functions for testing.
    Check suction cup end effector.

--- Classes/OmronTM5.700/@OmronTM5700/OmronTM5700.m
+++ Classes/OmronTM5.700/@OmronTM5700/OmronTM5700.m
@@ -13,8 +13,9 @@ classdef OmronTM5700 < RobotBaseClass
             end
             self.model.base = self.model.base.T * baseTr;
             self.PlotAndColourRobot();  
-
-            % self.model.teach(); % Only for testing. Comment out later
+            
+            % Teach() can be used to interact with OmronTM5
+                % self.model.teach();
         end
 
 %% Create the robot model
@@ -26,6 +27,7 @@ classdef OmronTM5700 < RobotBaseClass
             L4 = Link('d', 0.106, 'a', 0,     'alpha', pi/2);
             L5 = Link('d', 0.106,     'a', 0,     'alpha', -pi/2);
             L6 = Link('d', 0.11315, 'a', 0,     'alpha', 0);
+            L7 = Link('d', 0.1092, 'a', 0,     'alpha', 0);
             
             % Incorporate joint limits
             L1.qlim = [-270 270]*pi/180;        % Datasheet TM5-700 (+/-270)    % Tested -180 180
@@ -34,6 +36,7 @@ classdef OmronTM5700 < RobotBaseClass
             L4.qlim = [-180 180]*pi/180;        % Datasheet TM5-700 (+/-180)    % Tested -180 180
             L5.qlim = [-90 90]*pi/180;          % Datasheet TM5-700 (+/-180)    % Tested -90 90
             L6.qlim = [-225 225]*pi/180;        % Datasheet TM5-700 (+/-225)    % Tested -180 180
+            L7.qlim = [0 0]*pi/180;        
         
             L1.offset = pi/2;
             L2.offset = -pi/2;
@@ -41,9 +44,50 @@ classdef OmronTM5700 < RobotBaseClass
             L4.offset = pi/2;
             L5.offset = 0;
             L6.offset = 0;
+            L7.offset = 0;
             
-            self.model = SerialLink([L1 L2 L3 L4 L5 L6], 'name', 'Omron TM5');
+            self.model = SerialLink([L1 L2 L3 L4 L5 L6 L7], 'name', 'Omron TM5');
         end
-     
+    %%  Teach function
+        function OmronTeach(self, q)
+            if nargin < 2
+                    q = [0 0 0 0 0 0 0];
+            end
+            self.model.teach(q);
+        end
+
+    %%  Fkine function
+        function OmronFkine(self,q)
+            if nargin < 2
+                    q = [0 0 0 0 0 0 0];
+            end
+            OmronPose = self.model.fkine(q) %#ok<NOPRT,NASGU>
+        end
+
+    %% Move Omron function
+    function OmronMoveToCartesian(self,FinalCartesian)
+            % 
+            if nargin < 2
+                FinalCartesian = [-0.5, 0, 0];    % Default state
+            end
+            % Get initial pos
+                initialq = self.model.getpos;
+            % Get final transform
+                finalTr = transl(FinalCartesian) * rpy2tr(0, 180, 180, 'deg');
+            % Use inverse kinematics to find q - joint angles for target coordinates. 
+                finalq = self.model.ikcon(finalTr,initialq);
+            % Calculate using Trapezoidal Velocity Profile
+                steps = 50;
+                s = lspb(0,1,steps);
+                qMatrix = nan(steps, 7);        % 50 by 7 matrix with NaN
+                for i = 1:steps
+                    qMatrix(i,:) = ((1-s(i))*initialq) + (s(i)*finalq);
+                end
+            % Animate movement to pickup zone
+            for i = 1:6:steps
+                self.model.animate(qMatrix(i,:));
+                drawnow();
+            end
+        end
     end
 end
