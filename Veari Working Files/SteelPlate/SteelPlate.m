af057e47211c49397d4315df5345c561de586cef
VKila24 <160553306+VKila24@users.noreply.github.com>
2024-10-28 14:38:06 +1100

    Test Movement of Base Plate

--- Veari Working Files/SteelPlate/SteelPlate.m
+++ Veari Working Files/SteelPlate/SteelPlate.m
@@ -1,71 +1,58 @@
-classdef SteelPlate < RobotBaseClass
-    %% SteelPlate
-    properties(Access = public)              
-        plyFileNameStem = 'SteelPlate';
+classdef SteelPlate < handle
+    % STEELPLATE A class that creates a steel plate object
+    % The steel plate can be plotted at a specific location.
+    
+    properties
+        %> A steel plate model
+        steelplateModel;
+        
+        %> Base pose for the steel plate
+        basePose;
     end
     
     methods
-%% Define robot Function 
-        function self = SteelPlate(baseTr)
-			self.CreateModel();
+        %% Constructor
+        function self = SteelPlate(base)
             if nargin < 1			
-				baseTr = eye(4);				
+				base = [0 0 0];				
             end
-            self.model.base = self.model.base.T * baseTr;
-            self.PlotAndColourRobot();  
+            % Set the base location for the steel plate
+                self.basePose = SE3(SE2(base(1), base(2), 0));
+                self.steelplateModel = self.GetPlateModel('SteelPlate');
+
+            % Plot the steel plate model at the specified base location
+                self.Plot();
         end
-
-%% Create the SteelPlate model
-        function CreateModel(self)
-
-            L1 = Link('d', 1, 'a', 0, 'alpha', pi/2);
-            
-            self.model = SerialLink([L1], 'name', 'Steel Plate');
+        
+        function delete(self)
+            handles = findobj('Tag', self.steelplateModel.name);
+            h = get(handles, 'UserData');
+            try delete(h.robot); end
+            try delete(h); end
+            try delete(handles); end
         end
-
-    %%  Fkine function
-        % function SteelPlateFkine(self,q)
-        %     if nargin < 2
-        %             q = [0 0 0 0 0 0 0];
-        %     end
-        %     SteelPlatePose = self.model.fkine(q) %#ok<NOPRT,NASGU>
-        % end
-
-    %% Move SteelPlate function
-    % function SteelPlateMoveToCartesian(self,FinalCartesian)
-    %         % 
-    %         if nargin < 2
-    %             FinalCartesian = [-0.5, 0, 0];    % Default state
-    %         end
-    %         % Get initial pos
-    %             initialq = self.model.getpos;
-    %         % Get final transform
-    %             finalTr = transl(FinalCartesian) * rpy2tr(0, 0, 0, 'deg');
-    %         % Use inverse kinematics to find q - joint angles for target coordinates. 
-    %             finalq = self.model.ikcon(finalTr,initialq);
-    %         % Calculate using Trapezoidal Velocity Profile
-    %             steps = 35;
-    %             s = lspb(0,1,steps);
-    %             qMatrix = nan(steps, 7);        % 50 by 7 matrix with NaN
-    %             for i = 1:steps
-    %                 qMatrix(i,:) = ((1-s(i))*initialq) + (s(i)*finalq);
-    %             end
-    %         % Animate movement to pickup zone
-    %         for i = 1:10:steps
-    %             self.model.animate(qMatrix(i,:));
-    %             drawnow();
-    %         end
-    % end
-
-    %% Move SteelPlate Base Location
-    % function SteelPlateMoveBase(self,FinalCartesian)
-    %     % 
-    %         if nargin < 2
-    %             FinalCartesian = [-0.5, -0.5, 0.1];    % Default state
-    %         end
-    %         self.model.base = self.model.base.T * transl(FinalCartesian);
-    %         self.PlotAndColourRobot();
-    % end
-
+        
+        %% Plot the steel plate
+        function Plot(self)
+            plot3d(self.steelplateModel, 0, 'base', self.basePose, 'noarrow');
+            hold on;
+            axis equal
+            if isempty(findobj(get(gca, 'Children'), 'Type', 'Light'))
+                camlight;
+            end
+        end
+    end
+    
+    methods (Static)
+        %% GetPlateModel
+        function model = GetPlateModel(name)
+            if nargin < 1
+                name = 'SteelPlate';
+            end
+            [faceData, vertexData] = plyread('SteelPlateLink0.ply', 'tri');
+            model = SerialLink(Link('alpha', pi/2, 'a', 0, 'd', 0.1, 'offset', 0), 'name', name);
+            model.faces = {[], faceData};
+            model.points = {[], vertexData};
+        end
     end
 end
